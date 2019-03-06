d8e8a80bc367a6e4038f7fdf2f022a56db89e74c
Himanshu Nag <himanshu_nag@yahoo.com>
2019-03-06 20:41:26 +0530

    First Commit
    Upload project

---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controller Class
+/// </summary>
+[RequireComponent(typeof(InputManager))]
+public class Controller : MonoBehaviour
+{
+    /// <summary>
+    /// Refrence to InputManager
+    /// </summary>
+    InputManager input;
+
+	private void Awake()
+	{
+        input = GetComponent<InputManager>();
+	}
+
+    // Add your code below this bool condition => InputManager.Check("String(KeyBinding Command)") 
+	void Update()
+    {
+        if (input.Check("Shoot"))
+            gameObject.SetActive(false);
+        if (input.Check("Jump"))
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    /// <summary>
+    /// List for keybinding
+    /// </summary>
+    [SerializeField]
+    public List<Keys> keyBindings;
+
+    /// <summary>
+    /// To store key and input name as a databse.
+    /// </summary>
+    private Dictionary<string, KeyCode> keyDatabase;
+
+    private void Awake()
+    {
+        keyDatabase = new Dictionary<string, KeyCode>(keyBindings.Count);
+        for (int i = 0; i < keyBindings.Count; i++)
+        {
+            if (string.IsNullOrEmpty(keyBindings[i].Name))
+                throw new Exception("Please check all the assigned names and keys in InputManager script."); 
+            keyDatabase.Add(keyBindings[i].Name, keyBindings[i].keyCode);
+        }
+    }
+
+    /// <summary>
+    /// returns true if key is pressed for specific input.
+    /// </summary>
+    /// <returns>bool</returns>
+    /// <param name="inputName">Input name in key binding</param>
+    public bool Check(string inputName)
+    {
+        if (!keyDatabase.ContainsKey(inputName))
+            return false;
+        if (Input.GetKeyDown(keyDatabase[inputName]))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+/// <summary>
+/// Key class
+/// </summary>
+[System.Serializable]
+public class Keys
+{
+    public string Name;
+    public KeyCode keyCode;
+}
